bc1cd9ca07578390b1cc759528d838a54149b79b
Collins-k14 <collinskarani017@gmail.com>
2025-08-01 16:29:08 +0300

    added a comment

--- Calculator.py
+++ Calculator.py
@@ -14,11 +14,11 @@ elif operation == "*":
     result = num1 * num2  #Perform multiplication
     print(f"{num1} * {num2} = {result}")
 elif operation == "/":
-#check if the second number is zero 
+#check if the second number is zero 1
     if num2 == 0:
         print("Error: Division by zero is not allowed.")
     else:
-        result = num1 / num2
+        result = num1 / num2 #Perform division 
         print(f"{num1} / {num2} = {result}")
 else:
     print("Invalid operation. Please enter one of +, -, *, /.")
